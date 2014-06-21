c82b298f3ba39499032f2bd0bfba32eb6ca2a41c
Matthew Dapena-Tretter <m@tthewwithanm.com>
2014-06-20 22:13:49 -0400

    Some initial Route/Router work

--- test/browser.js
+++ test/browser.js
@@ -1,9 +1,26 @@
 var assert = chai.assert;
-var Router = ReactRouting.Router;
+var noop = function() {};
+var Route = ReactRouting.Route;
 
 
-describe('react-routing', function () {
-    it('passes a test', function () {
-        assert(true);
-    })
+describe('react-routing', function() {
+  describe('createRouterClass', function() {
+
+    it('creates a component class', function() {
+      var Router = ReactRouting.createRouterClass();
+      assert(React.isValidClass(Router));
+    });
+
+    it('creates routes from the arguments', function() {
+      var Router = ReactRouting.createRouterClass(
+        {name: 'a', path: 'a', handler: noop},
+        {name: 'b', path: 'b', handler: noop}
+      );
+      assert.equal(Router.routes.length, 2);
+      Router.routes.forEach(function(route) {
+        assert.instanceOf(route, Route);
+      });
+    });
+
+  });
 });

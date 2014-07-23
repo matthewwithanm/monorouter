465fd3c3629a7762c0df7b0566941a60bcda218d
Eric Eldredge <lettertwo@gmail.com>
2014-07-22 22:07:40 -0400

    Update server example router to reflect API changes

--- examples/server/router.js
+++ examples/server/router.js
@@ -1,24 +1,15 @@
 var monorouter = require('../../lib');
 var App = require('../build/server');
 
-var Router = monorouter.createRouterClass();
-Router
-  .route({
-    name: 'index',
-    path: '',
-    handler: function() {
-      this.setView(App);
-    }
-  })
-  .route({
-    name: 'pet',
-    path: 'pet/:name',
-    handler: function() {
-      var petName = this.request.params.name;
-      this.setView(function() {
-        return App({petName: petName});
-      });
-    }
-  });
 
-module.exports = Router;
+module.exports = monorouter()
+  .route('index', '', function(req) {
+    console.log('routing...');
+    this.render(App);
+  })
+  .route('pet', 'pet/:name', function(req) {
+    this
+      .setView(App)
+      .setState({petName: req.params.name})
+      .end();
+  });
